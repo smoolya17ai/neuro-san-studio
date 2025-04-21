1005ec4e807a21c5d784d90515ee2c1c757f6731
Hormoz Shahrzad <hshahrzad@yahoo.com>
2025-04-20 21:09:53 -0500

    Removed async from the agents needed to be called in a sequence.

--- coded_tools/agent_network_designer/get_agent_network.py
+++ coded_tools/agent_network_designer/get_agent_network.py
@@ -1,12 +1,3 @@
-# Copyright (C) 2023-2025 Cognizant Digital Business, Evolutionary AI.
-# All Rights Reserved.
-# Issued under the Academic Public License.
-#
-# You can be released from the terms, and requirements of the Academic Public
-# License by purchasing a commercial license.
-# Purchase of a commercial license is mandatory for any use of the
-# neuro-san-demos SDK Software in commercial settings.
-#
 from typing import Any
 from typing import Dict
 from typing import Union
@@ -25,7 +16,7 @@ class GetAgentNetwork(CodedTool):
     def __init__(self):
         self.agents = None
 
-    async def async_invoke(self, args: Dict[str, Any], sly_data: Dict[str, Any]) -> Union[Dict[str, Any], str]:
+    def invoke(self, args: Dict[str, Any], sly_data: Dict[str, Any]) -> Union[Dict[str, Any], str]:
         """
         :param args: An argument dictionary whose keys are the parameters
                 to the coded tool and whose values are the values passed for them
