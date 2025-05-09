199664cd1ae80b968cb5afc6222d73d07c615974
Olivier Francon <olivier.francon@cognizant.com>
2025-05-09 16:26:25 -0700

    Bump service version to 0.0.9

--- deploy/build.sh
+++ deploy/build.sh
@@ -20,7 +20,7 @@
 #
 
 export SERVICE_TAG=${SERVICE_TAG:-neuro-san-demos}
-export SERVICE_VERSION=${SERVICE_VERSION:-0.0.8}
+export SERVICE_VERSION=${SERVICE_VERSION:-0.0.9}
 
 function check_directory() {
     working_dir=$(pwd)
