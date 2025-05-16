37f7cfd4314a307732fbdf023ea3f50b50ff9ae8
Deepak Singh <deepak.singh10@cognizant.com>
2025-05-16 13:10:31 -0700

    update coded tool

--- coded_tools/agentspace_adapter/agentspace_adapter.py
+++ coded_tools/agentspace_adapter/agentspace_adapter.py
@@ -75,7 +75,7 @@ class AgentSpaceSearch(CodedTool):
         logger = logging.getLogger(self.__class__.__name__)
         logger.info(">>>>>>>>>>>>>>>>>>>WebsiteSearch>>>>>>>>>>>>>>>>>>")
         logger.info("Search Query: %s", str(search_query))
-        res = search_sample(self.project_id, self.location, self.engine_id, search_query)
+        res = self.search_sample(search_query)
         logger.info(">>>>>>>>>>>>>>>>>>>DONE !!!>>>>>>>>>>>>>>>>>>")
         return res
 
@@ -84,77 +84,71 @@ class AgentSpaceSearch(CodedTool):
         # For this example, we will just call the synchronous method
         return self.invoke(args, sly_data)
 
+    # Helper function to perform the search
+    def search_sample(self, search_query: str) -> discoveryengine.services.search_service.pagers.SearchPager:
+        """
+        Perform a search using the Discovery Engine API.
+        """
+        #  For more information, refer to:
+        # https://cloud.google.com/generative-ai-app-builder/docs/locations#specify_a_multi-region_for_your_data_store
+        client_options = (
+            ClientOptions(api_endpoint=f"{self.location}-discoveryengine.googleapis.com") if self.location != "global" else None
+        )
 
-# Helper function to perform the search
-def search_sample(
-    project_id: str,
-    location: str,
-    engine_id: str,
-    search_query: str,
-) -> discoveryengine.services.search_service.pagers.SearchPager:
-    """
-    Perform a search using the Discovery Engine API.
-    """
-    #  For more information, refer to:
-    # https://cloud.google.com/generative-ai-app-builder/docs/locations#specify_a_multi-region_for_your_data_store
-    client_options = (
-        ClientOptions(api_endpoint=f"{location}-discoveryengine.googleapis.com") if location != "global" else None
-    )
+        # Create a client
+        client = discoveryengine.SearchServiceClient(client_options=client_options)
 
-    # Create a client
-    client = discoveryengine.SearchServiceClient(client_options=client_options)
+        # The full resource name of the search app serving config
+        proj_loc = f"projects/{self.project_id}/locations/{self.location}/"
+        proj_loc_engine_id = f"{proj_loc}collections/default_collection/engines/{self.engine_id}/"
+        serving_config = f"{proj_loc_engine_id}/servingConfigs/default_config"
 
-    # The full resource name of the search app serving config
-    proj_loc = f"projects/{project_id}/locations/{location}/"
-    proj_loc_engine_id = f"{proj_loc}collections/default_collection/engines/{engine_id}/"
-    serving_config = f"{proj_loc_engine_id}/servingConfigs/default_config"
-
-    # Optional - only supported for unstructured data: Configuration options for search.
-    # Refer to the `ContentSearchSpec` reference for all supported fields:
-    # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1.types.SearchRequest.ContentSearchSpec
-    content_search_spec = discoveryengine.SearchRequest.ContentSearchSpec(
-        # For information about snippets, refer to:
-        # https://cloud.google.com/generative-ai-app-builder/docs/snippets
-        snippet_spec=discoveryengine.SearchRequest.ContentSearchSpec.SnippetSpec(return_snippet=True),
-        # For information about search summaries, refer to:
-        # https://cloud.google.com/generative-ai-app-builder/docs/get-search-summaries
-        summary_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec(
-            summary_result_count=5,
-            include_citations=True,
-            ignore_adversarial_query=True,
-            ignore_non_summary_seeking_query=True,
-            model_prompt_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelPromptSpec(
-                preamble="YOUR_CUSTOM_PROMPT"
+        # Optional - only supported for unstructured data: Configuration options for search.
+        # Refer to the `ContentSearchSpec` reference for all supported fields:
+        # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1.types.SearchRequest.ContentSearchSpec
+        content_search_spec = discoveryengine.SearchRequest.ContentSearchSpec(
+            # For information about snippets, refer to:
+            # https://cloud.google.com/generative-ai-app-builder/docs/snippets
+            snippet_spec=discoveryengine.SearchRequest.ContentSearchSpec.SnippetSpec(return_snippet=True),
+            # For information about search summaries, refer to:
+            # https://cloud.google.com/generative-ai-app-builder/docs/get-search-summaries
+            summary_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec(
+                summary_result_count=5,
+                include_citations=True,
+                ignore_adversarial_query=True,
+                ignore_non_summary_seeking_query=True,
+                model_prompt_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelPromptSpec(
+                    preamble="YOUR_CUSTOM_PROMPT"
+                ),
+                model_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelSpec(
+                    version="stable",
+                ),
             ),
-            model_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelSpec(
-                version="stable",
+        )
+
+        # Refer to the `SearchRequest` reference for all supported fields:
+        # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1.types.SearchRequest
+        request = discoveryengine.SearchRequest(
+            serving_config=serving_config,
+            query=search_query,
+            page_size=10,
+            content_search_spec=content_search_spec,
+            query_expansion_spec=discoveryengine.SearchRequest.QueryExpansionSpec(
+                condition=discoveryengine.SearchRequest.QueryExpansionSpec.Condition.AUTO,
             ),
-        ),
-    )
+            spell_correction_spec=discoveryengine.SearchRequest.SpellCorrectionSpec(
+                mode=discoveryengine.SearchRequest.SpellCorrectionSpec.Mode.AUTO
+            ),
+            # Optional: Use fine-tuned model for this request
+            # custom_fine_tuning_spec=discoveryengine.CustomFineTuningSpec(
+            #     enable_search_adaptor=True
+            # ),
+        )
 
-    # Refer to the `SearchRequest` reference for all supported fields:
-    # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1.types.SearchRequest
-    request = discoveryengine.SearchRequest(
-        serving_config=serving_config,
-        query=search_query,
-        page_size=10,
-        content_search_spec=content_search_spec,
-        query_expansion_spec=discoveryengine.SearchRequest.QueryExpansionSpec(
-            condition=discoveryengine.SearchRequest.QueryExpansionSpec.Condition.AUTO,
-        ),
-        spell_correction_spec=discoveryengine.SearchRequest.SpellCorrectionSpec(
-            mode=discoveryengine.SearchRequest.SpellCorrectionSpec.Mode.AUTO
-        ),
-        # Optional: Use fine-tuned model for this request
-        # custom_fine_tuning_spec=discoveryengine.CustomFineTuningSpec(
-        #     enable_search_adaptor=True
-        # ),
-    )
+        page_result = client.search(request)
 
-    page_result = client.search(request)
+        # Handle the response
+        for response in page_result:
+            print(response)
 
-    # Handle the response
-    for response in page_result:
-        print(response)
-
-    return page_result
+        return page_result
