442075080f5d658a9328ea2f8874e353951b37b2
Noravee Kanchanavatee <noraveek@gmail.com>
2025-07-31 02:33:57 -0700

    Add openai search and code interpreter

--- coded_tools/openai_tool.py
+++ coded_tools/openai_tool.py
@@ -17,14 +17,12 @@ from langchain_core.messages import AIMessage
 from langchain_openai import ChatOpenAI
 from openai import OpenAIError
 
-from neuro_san.interfaces.coded_tool import CodedTool
-
 DEFAULT_OPENAI_MODEL = "gpt-4o-2024-08-06"
 
 
-class OpenaiTool(CodedTool):
+class OpenAITool:
     """
-    A CodedTool implementation for invoking OpenAI built-in tools using LangChain's ChatOpenAI.
+    An implementation for invoking OpenAI built-in tools using LangChain's ChatOpenAI.
 
     Supported tools include (but are not limited to):
         - "code_interpreter"
@@ -35,83 +33,60 @@ class OpenaiTool(CodedTool):
 
     Only "code_interpreter" and "web_search_preview" have been tested.
 
-    See: https://platform.openai.com/docs/guides/tools?api-mode=responses
+    See https://platform.openai.com/docs/guides/tools?api-mode=responses
     """
 
-    async def async_invoke(self, args: dict[str, Any], sly_data: dict[str, Any]) -> list[dict[str, Any]] | str:
+    logger = logging.getLogger(__name__)
+
+    @staticmethod
+    async def arun(
+        query: str,
+        builtin_tool: str,
+        openai_model: str | None = DEFAULT_OPENAI_MODEL,
+        **additional_kwargs: dict[str, Any]
+    ) -> list[dict[str, Any]] | str:
         """
-        :param args: An argument dictionary whose keys are the parameters
-                to the coded tool and whose values are the values passed for them
-                by the calling agent.  This dictionary is to be treated as read-only.
-
-                The argument dictionary expects the following keys:
-                    "query"
-
-        :param sly_data: A dictionary whose keys are defined by the agent hierarchy,
-                but whose values are meant to be kept out of the chat stream.
-
-                This dictionary is largely to be treated as read-only.
-                It is possible to add key/value pairs to this dict that do not
-                yet exist as a bulletin board, as long as the responsibility
-                for which coded_tool publishes new entries is well understood
-                by the agent chain implementation and the coded_tool implementation
-                adding the data is not invoke()-ed more than once.
-
-                Keys expected for this implementation are:
-                    None
+        :param query: Request from the user prompt.
+        :param builtin_tool: The name of the built-in OpenAI tool to invoke.
+        :param openai_model: The OpenAI model to use when calling the tool.
+            Defaults to "gpt-4o-2024-08-06" if not provided.
+        :param additional_kwargs: Additional keyword arguments to pass to the selected tool.
+            Tool-specific requirements:
+                - "web_search_preview": no additional kwargs needed.
+                - "code_interpreter": requires "container" to be set.
+            Refer to https://python.langchain.com/docs/integrations/chat/openai/#responses-api
 
         :return:
             In case of successful execution:
-                A list of dictionary of search results
+                Tool results
             otherwise:
                 a text string an error message in the format:
-                "Error: <error message>"
+                "OpenAI Error: <error message>"
         """
 
-        # Get query from args
-        query: str = args.get("query")
-        if not query:
-            return "Error: No query provided."
+        if not openai_model:
+            openai_model = DEFAULT_OPENAI_MODEL
 
-        # User-defined arguments
-
-        # # The name of the built-in OpenAI tool to invoke (Required).
-        builtin_tool: str = args.get("builtin_tool")
-        if not builtin_tool:
-            return "Error: No builtin tool specified."
-
-        # The OpenAI model to use when calling the tool.
-        # Defaults to "gpt-4o-2024-08-06" if not provided.
-        openai_model: str = args.get("openai_model", DEFAULT_OPENAI_MODEL)
-
-        # Additional keyword arguments to pass to the selected tool.
-        # Tool-specific requirements:
-        #   - "web_search_preview": no additional kwargs needed.
-        #   - "code_interpreter": requires "container" to be set.
-        # Refer to:
-        # https://python.langchain.com/docs/integrations/chat/openai/#code-interpreter
-        additional_kwarg: dict[str, Any] = args.get("additional_kwarg", {})
-
-        logger = logging.getLogger(self.__class__.__name__)
-        logger.info(">>>>>>>>>> Invoking OpenAI Tool <<<<<<<<<<")
-        logger.info("Query: %s", query)
-        logger.info("OpenAI Model: %s", openai_model)
-        logger.info("Built-in Tool: %s", builtin_tool)
-        logger.info("Additional Keyword Arguments: %s", additional_kwarg)
+        OpenAITool.logger.info(">>>>>>>>>> Invoking OpenAI Tool <<<<<<<<<<")
+        OpenAITool.logger.info("Query: %s", query)
+        OpenAITool.logger.info("OpenAI Model: %s", openai_model)
+        OpenAITool.logger.info("Built-in Tool: %s", builtin_tool)
+        OpenAITool.logger.info("Additional Keyword Arguments: %s", additional_kwargs)
 
         try:
             # Instantiate the chat model using specified model.
             # The "output_version" key format output from built-in tool invocations into
             # the message’s content field, rather than additional_kwargs.
             openai_llm = ChatOpenAI(model=openai_model, output_version="responses/v1")
-            tool: dict[str, Any] = {"type": builtin_tool} | additional_kwarg
+            tool: dict[str, Any] = {"type": builtin_tool} | additional_kwargs
 
-            # Invoke with the provided query and tool, "tool_choice" is set to "required" to force the model to use tool.
+            # Invoke with the provided query and tool,
+            # "tool_choice" is set to "required" to force the model to use tool.
             result: AIMessage = await openai_llm.ainvoke(query, tools=[tool], tool_choice="required")
             content: list[dict[str, Any]] = result.content
-            logger.info("Result from OpenAI Tool: %s", content)
+            OpenAITool.logger.info("Result from OpenAI Tool: %s", content)
             return content
 
         except OpenAIError as openai_error:
-            logger.error("OpenAI Error: %s", openai_error)
+            OpenAITool.logger.error("OpenAI Error: %s", openai_error)
             return f"OpenAI Error: {openai_error}"
